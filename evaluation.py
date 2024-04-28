24d7a81cbe211fa6f611d0da335b70a87fd54863
Lehito15 <266877@student.pwr.edu.pl>
2024-04-28 02:32:02 +0200

    V3

--- evaluation.py
+++ evaluation.py
@@ -8,13 +8,14 @@ def manhatan_2(point_x, point_y, x, y, board, base, player_type):
     else:
         empty_base = all(board[x][y] == 0 or board[x][y] == 1 for x, y in base)
     if empty_base:
-       # print('empty',player_type)
-       return manhatan(point_x, point_y, x, y)
+        # print('empty',player_type)
+        return manhatan(point_x, point_y, x, y)
     else:
         if board[x][y] in base:
             return abs(point_x - x) + abs(point_y - y)
         else:
-            return abs(point_x - x) + abs(point_y - y) *5
+            return abs(point_x - x) + abs(point_y - y) * 5
+
 
 def if_in_base(player_type, base, board, x, y):
     if (x, y) in base:
@@ -23,6 +24,8 @@ def if_in_base(player_type, base, board, x, y):
         else:
             return 100
     return 0
+
+
 def leave_base(player_type, state, base):
     if state[0]:
         if state[0][0][0] in base:
@@ -31,20 +34,22 @@ def leave_base(player_type, state, base):
                     return 4
                 else:
                     return -4
-        return  0
+        return 0
     return 0
+
 
 def rival_base(player_type, board, base, x, y):
     if (x, y) in base:
         if player_type == 1:
-            return  5
+            return 5
         else:
             return -5
-    return  0
+    return 0
+
 
 def go_in_group(player_type, board, base, x, y):
     value = 0
-    if (x,y) not in base:
+    if (x, y) not in base:
         for i in range(-1, 2):
             for j in range(-1, 2):
                 if board[i][j] == player_type:
@@ -60,6 +65,3 @@ def go_in_group(player_type, board, base, x, y):
     # else:
     #     value = 4
     return value
-
-
-
